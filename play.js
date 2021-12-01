012ab0b7adb5304849eacd2eb2221a8bbb778b23
ZaraAhKhan <88912965+NorwegianRidgeback@users.noreply.github.com>
2021-12-01 17:03:03 -0500

    userIput setup

--- play.js
+++ play.js
@@ -1,4 +1,30 @@
 const {connect} = require('./client');
+const readline = require("readline");
+const { Socket } = require('dgram');
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+  
+};
 
 
+const handleUserInput = function() {
+  // your code here
+  process.stdin.on("data", (key) => {
+    if (key === ("\x{3}") {
+      connect.disconnect();
+      process.exit();
+    }
+    
+  });
+
+  
+};
+
+setupInput();
 
